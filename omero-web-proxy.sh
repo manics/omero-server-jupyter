31561007d17485d7517d7e77622415c6ecf06ddd
Simon Li <orpheus+devel@gmail.com>
2019-01-05 23:34:56 +0000

    Patch OMERO.web to take extensible additional django config

--- omero-web-proxy.sh
+++ omero-web-proxy.sh
@@ -15,7 +15,33 @@ omero=/opt/omero/server/OMERO.server/bin/omero
 #$omero config set omero.web.prefix $BASE_URL/omeroweb
 #$omero config set omero.web.static_url $BASE_URL/omeroweb/static/
 #$omero config set omero.web.prefix $BASE_URL/proxy/$PORT
-sed -i 's%WHITENOISE_STATIC_PREFIX =.*%WHITENOISE_STATIC_PREFIX = "/static/"%' /opt/omero/server/OMERO.server/lib/python/omeroweb/settings.py
+
+$omero config append --set omero.web.django_additional_settings '["WHITENOISE_STATIC_PREFIX", "/static/"]'
+$omero config append --set omero.web.django_additional_settings '["LOGGING",
+  {
+    "version": 1,
+    "disable_existing_loggers": false,
+    "formatters": {
+      "standard": {
+        "format": "%(asctime)s %(levelname)5.5s [%(name)40.40s] (proc.%(process)5.5d) %(funcName)s():%(lineno)d %(message)s"
+      }
+    },
+    "handlers": {
+      "console": {
+        "level": "DEBUG",
+        "class": "logging.StreamHandler",
+        "formatter": "standard"
+      }
+    },
+    "loggers": {
+      "": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+        "propagate": true
+      }
+    }
+  }
+]'
 
 $omero config set omero.web.static_url $BASE_URL/proxy/$PORT/static/
 $omero config set omero.web.application_server.port $PORT
